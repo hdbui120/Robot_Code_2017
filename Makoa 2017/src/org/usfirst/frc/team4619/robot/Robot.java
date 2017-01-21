0c33ab562bc2c5e8c641f51716e8c05651d6a95b
tony-phan <Robotics@sw03.OVHS.HBUHSD.AD>
2017-01-20 16:16:47 -0800

    Added DriveBase
    Declared & initialized drivebase in robot.java class

--- Makoa 2017/src/org/usfirst/frc/team4619/robot/Robot.java
+++ Makoa 2017/src/org/usfirst/frc/team4619/robot/Robot.java
@@ -9,6 +9,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import org.usfirst.frc.team4619.robot.commands.ExampleCommand;
+import org.usfirst.frc.team4619.robot.subsystems.DriveBase;
 import org.usfirst.frc.team4619.robot.subsystems.ExampleSubsystem;
 
 /**
@@ -22,6 +23,7 @@ public class Robot extends IterativeRobot {
 
 	public static final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
 	public static OI oi;
+	public static DriveBase driveBase;
 
 	Command autonomousCommand;
 	SendableChooser<Command> chooser = new SendableChooser<>();
@@ -36,6 +38,7 @@ public class Robot extends IterativeRobot {
 		chooser.addDefault("Default Auto", new ExampleCommand());
 		// chooser.addObject("My Auto", new MyAutoCommand());
 		SmartDashboard.putData("Auto mode", chooser);
+		driveBase = new DriveBase();
 	}
 
 	/**
@@ -67,7 +70,7 @@ public class Robot extends IterativeRobot {
 	@Override
 	public void autonomousInit() {
 		autonomousCommand = chooser.getSelected();
-
+		
 		/*
 		 * String autoSelected = SmartDashboard.getString("Auto Selector",
 		 * "Default"); switch(autoSelected) { case "My Auto": autonomousCommand
