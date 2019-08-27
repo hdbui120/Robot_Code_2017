d983d4c984c8e82819cb60956bcf33426f49fca1
Hieu Bui <hieubui457@gmail.com>
2019-08-27 02:47:38 -0700

    add descriptions to Climber subsystem class in subsystems directory

--- Makoa 2017/src/org/usfirst/frc/team4619/robot/subsystems/Climber.java
+++ Makoa 2017/src/org/usfirst/frc/team4619/robot/subsystems/Climber.java
@@ -7,11 +7,20 @@ import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
+/*
+	This class construct the climber mechanism 
+	The climber is used at the end of the match to get the final points
+	The climber uses two mini-SIM motor to climb
+	It stops when the limit switch is activated
+*/
+
 public class Climber extends Subsystem
 {
-	DigitalInput limitSwitch;
-	VictorSP motor;
-	VictorSP motor2;
+	//declare essetial objects for climber mechanism
+	//1 limit switch, 2 victor motor, and 2 variables to control the motors
+	private static DigitalInput limitSwitch;
+	private static VictorSP motor;
+	private static VictorSP motor2;
 	double climbSpeed;
 	double zeroSpeed = 0;
 	
@@ -19,31 +28,37 @@ public class Climber extends Subsystem
 	protected void initDefaultCommand() 
 	{
 		// TODO Auto-generated method stub
+		//set the default state to doing nothing, climb speed = 0
 		setDefaultCommand(new StopClimb());
 	}	
 	
+	//constructor that initialize motors and limit switch
 	public Climber()
 	{
 		motor = new VictorSP(RobotMap.PWM_PORT_4);
 		motor2 = new VictorSP(RobotMap.PWM_PORT_6);
-		//limitSwitch = new DigitalInput(4);
+		limitSwitch = new DigitalInput(4);
 	}
 	
-	/*public DigitalInput getLimitSwitch()
+	//get method for acquiring the limitSwitch
+	public DigitalInput getLimitSwitch()
 	{
 		return limitSwitch;
-	}*/
+	}
 	
+	//get method for acquiring motor 1
 	public VictorSP getMotor()
 	{
 		return motor;
 	}
 	
+	//get method for acquiring motor 2
 	public VictorSP getMotor2()
 	{
 		return motor2;
 	}
 	
+	//make climber climbs at parameter input speed
 	public void Climb(double climbSpeed)
 	{
 		this.climbSpeed = climbSpeed;
@@ -51,12 +66,14 @@ public class Climber extends Subsystem
 			motor2.set(climbSpeed);
 	}
 	
+	//make the climber do nothing
 	public void StopClimb()
 	{
 		motor.set(zeroSpeed);
 		motor2.set(zeroSpeed);
 	}
 	
+	//make the climber do nothing
 	public void doNothing() 
 	{
 		motor.set(zeroSpeed);
